5c37f91db648f072a504321813be1396234b547a
neuhaus <neuhaus@neuhaus-note.(none)>
2012-07-11 02:41:47 -0300

    adicionados acentos aos arquivos eps

--- figures/scripts/prbs.m
+++ figures/scripts/prbs.m
@@ -15,4 +15,4 @@ stairs(u.*2-1)
 axis([0 N -1.5 1.5])
 xlabel(['Sinal PRBS com N=' int2str(N) ' M=127']);
 ylabel('Amplitude de u(t)');
-title('Sinal pseudo randomico binario para n=7. u(t)=rem(u(t-3)+u(t-7), 2)');
+title('Sinal pseudo randomico binário para n=7. u(t)=rem(u(t-3)+u(t-7), 2)');
